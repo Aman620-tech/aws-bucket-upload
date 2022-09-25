e38163cdcddcd5fd11c3a79e56976b3e32cb71e8
AMAN KUMAR PANDEY <76736496+Aman620-tech@users.noreply.github.com>
2022-09-25 22:19:28 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -2,11 +2,12 @@
 const { upload } = require("./fileupload");
 const register = async (req, res) => {
   const { firstName, email } = req.body;
-  //
-  //   console.log(req.body);
-  const image = req.files["image"];
-  extensions = [".png", ".jpg", ".jpeg"];
-  //
+  if (req.files && req.files["image"]) {
+    const extensions = [".png", ".jpg", ".jpeg"];
+    const link = await upload(req.files["image"], extensions);
+    const data = { email, firstName, profile: link };
+    res.json({ email, fileLikn });
+  }
   const link = await upload(image, extensions);
 
   const data = { email, firstName, profile: link };
